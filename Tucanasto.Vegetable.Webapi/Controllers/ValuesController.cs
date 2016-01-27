23b79ba7329e1f4827ce354e154e530ca703087b
andresvalar@hotmail.com <andresvalar@hotmail.com>
2016-01-28 08:23:44 +1100

    first

--- Tucanasto.Vegetable.Webapi/Controllers/ValuesController.cs
+++ Tucanasto.Vegetable.Webapi/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace Tucanasto.Vegetable.Webapi.Controllers
 		// GET api/values
 		public IEnumerable<string> Get()
 		{
-			return new string[] { "value1", "value2" };
+			return new string[] { "value1Carlos", "value2" };
 		}
 
 		// GET api/values/5
